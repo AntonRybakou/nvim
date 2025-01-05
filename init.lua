baf990ef762dcbe9b6a9111e1251cf49fc45553f
Anton Rybakou <Anton Rybakou>
2025-01-05 21:41:21 +0300

    feat: add search files with <leader>sf

--- init.lua
+++ init.lua
@@ -191,7 +191,7 @@ require("lazy").setup({
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-			-- vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
 			vim.keymap.set("n", "<D-p>", builtin.find_files, { desc = "Search files with 'Cmd + P'" })
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
@@ -550,8 +550,8 @@ require("lazy").setup({
 
 	require("plugins.autopairs"),
 	require("plugins.neo-tree"),
-	require("plugins.catppuccin"),
-	-- require("plugins.onedark"),
+	-- require("plugins.catppuccin"),
+	require("plugins.onedark"),
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
